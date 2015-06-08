99fca973929252e039ce120e98e8f4a841454270
seb <seb@csdev-seb.SMK.DK>
2015-06-08 17:11:09 +0200

    [!!!] date range - commit with error

--- widgets/DateRangeWidget.js
+++ widgets/DateRangeWidget.js
@@ -2,14 +2,16 @@
 
 	AjaxSolr.DateRangeWidget = AjaxSolr.AbstractFacetWidget.extend({	  
 
-		init: function () {
+		beforeRequest: function () {
 
 			var self = this;
 			var $target = $(this.target);		  		  
-			var html = self.template_integration_json({}, '#daterangeTemplate');
+			var html = self.template_integration_json({label: this.manager.translator.getLabel('daterange_label') ,
+														from: smkCommon.firstCapital(this.manager.translator.getLabel('daterange_from')),
+														to: smkCommon.firstCapital(this.manager.translator.getLabel('daterange_to')),
+														show: smkCommon.firstCapital(this.manager.translator.getLabel('daterange_show'))}, '#daterangeTemplate');
 			$target.html(html);	  	  	  			
 
-			// user validates a search string (lower priority than dropdown list)
 			$(self.target).find('form').submit(function (e) {				
 				event.preventDefault();
 				$(self).trigger({
@@ -17,11 +19,30 @@
 					params: {date_range: self.format_request(self)}					
 				});											
 			});
+			
+			this.update_filters();
 		},
+		
+		update_filters: function(){
+			var self = this;
+			var facets = ModelManager.get_facets_lab_for_search_component();
 
-		beforeRequest: function () {
-//			$(this.target).find('label').html('<i></i>' + smkCommon.firstCapital(this.manager.translator.getLabel('checkbox_hasimage')));						
-//			$(this.target).find('input').prop('checked', ModelManager.get_hasimage());
+			// add current filters 
+			if (facets !== undefined){	
+				for (var i = 0, l = facets.length; i < l; i++) {					
+					$.each(self.field, function( key, value ){
+						if(facets[i].id == key){
+							var date = facets[i].text.match(/\[(.*)-01-01T00:00:00.001Z TO/);
+							if(smkCommon.isValidDataText(date))
+								$(self.target).find('[name='+ key +']').val(date[1]);
+							
+							date = facets[i].text.match(/TO (.*)-01-01T00:00:00.001Z\]/);
+							if(smkCommon.isValidDataText(date))
+								$(self.target).find('[name='+ key +']').val(date[1]);
+						}
+					});											
+				}
+			}			
 		},
 
 		template_integration_json: function (json_data, templ_id){	  
@@ -33,16 +54,16 @@
 		format_request: function(self){			
 			var res = {};
 			var date_format = '%s-01-01T00:00:00.001Z';	
-			var req_late_format = sprintf('%s:[%%s TO *]', self.field.late);
-			var req_earl_format = sprintf('%s:[* TO %%s]', self.field.earl);
+			var sup = sprintf('%s:[%%s TO *]', self.field.acq_date_earliest);
+			var inf = sprintf('%s:[* TO %%s]', self.field.acq_date_latest);
 			
-			var max = $(self.target).find('[name=latest]').val();
-			max = smkCommon.isValidDataText(max) ? sprintf(req_earl_format, sprintf(date_format, max)) : null;						
-			res[self.field.earl] = max;
+			var max = $(self.target).find('[name=acq_date_latest]').val();
+			max = smkCommon.isValidDataText(max) ? sprintf(sup, sprintf(date_format, max)) : null;						
+			res[self.field.acq_date_earliest] = max;
 		
-			var min = $(self.target).find('[name=earliest]').val();
-			min = smkCommon.isValidDataText(min) ? sprintf(req_late_format, sprintf(date_format, min)) : null;
-			res[self.field.late] = min;
+			var min = $(self.target).find('[name=acq_date_earliest]').val();
+			min = smkCommon.isValidDataText(min) ? sprintf(inf, sprintf(date_format, min)) : null;
+			res[self.field.acq_date_latest] = min;
 			
 			return res;
 		}
