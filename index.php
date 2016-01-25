c70cfe3a7816568ef49b773b016e3fac5baf4e80
seb <seb@csdev-seb.SMK.DK>
2016-01-25 12:53:47 +0100

    [FEATURE] Google Analytics code changed

--- index.php
+++ index.php
@@ -31,7 +31,15 @@
 	<script src="js/supplier/feed.js" type="text/javascript"></script>
 	<script src="js/supplier/dotdotdot.js" type="text/javascript"></script>
 	<script src="js/supplier/dotdotdot-config.js" type="text/javascript"></script>
-	<script src="js/supplier/ga.js" type="text/javascript"></script>
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TH787P"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-TH787P');</script>
+	<!-- End Google Tag Manager -->
 
 
 	
